b2b4b0c76fe94bddbe75844a27ef8070c3702805
Monojit Tewari <tewarimonojit@gmail.com>
2024-05-21 23:22:45 +0530

    job service and controller layer is implemented

---
+++ JobApplication/src/main/java/com/monojit/JobApplication/Job/JobRepository.java
@@ -0,0 +1,7 @@
+package com.monojit.JobApplication.Job;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job,Long> {
+
+}
--- JobApplication/src/main/java/com/monojit/JobApplication/Job/impl/JobServiceImpl.java
+++ JobApplication/src/main/java/com/monojit/JobApplication/Job/impl/JobServiceImpl.java
@@ -1,6 +1,7 @@
 package com.monojit.JobApplication.Job.impl;
 
 import com.monojit.JobApplication.Job.Job;
+import com.monojit.JobApplication.Job.JobRepository;
 import com.monojit.JobApplication.Job.JobService;
 import org.springframework.stereotype.Service;
 
@@ -10,32 +11,41 @@ import java.util.Optional;
 
 @Service
 public class JobServiceImpl implements JobService {
-    private List<Job> jobs = new ArrayList<>();
+//    private List<Job> jobs = new ArrayList<>();
+    JobRepository jobRepository;
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
     @Override
     public List<Job> findAll() {
-       return jobs;
+       return jobRepository.findAll();
     }
 
     @Override
     public void createJob(Job job) {
-        job.setId(Long.valueOf(jobs.size()));
-        jobs.add(job);
+        jobRepository.save(job);
     }
 
     @Override
     public Job getJobById(Long id) {
-        Optional<Job> job= jobs.stream().filter(j->j.getId().equals(id)).findFirst();
-        return job.orElse(null);
+        return jobRepository.findById(id).orElse(null);
     }
 
     @Override
     public Boolean removeJob(Long id) {
-        return jobs.removeIf(job->job.getId().equals(id));
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        }
+        catch (Exception e){
+            return  false;
+        }
     }
 
     @Override
     public Boolean updateJob(Long id, Job updatedJob) {
-        Optional<Job>existingJobOpt=jobs.stream().filter(j->j.getId().equals(id)).findFirst();
+        Optional<Job>existingJobOpt=jobRepository.findById(id);
         if(existingJobOpt.isPresent()){
             Job existingJob = existingJobOpt.get();
             existingJob.setTitle(updatedJob.getTitle());
@@ -43,6 +53,7 @@ public class JobServiceImpl implements JobService {
             existingJob.setMinSalary(updatedJob.getMinSalary());
             existingJob.setMaxSalary(updatedJob.getMaxSalary());
             existingJob.setLocation(updatedJob.getLocation());
+            jobRepository.save(existingJob);
             return true;
         }else {
             return false;
